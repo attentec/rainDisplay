a9a3c7fcbc0871dee2d419bc434b685acc14646f
maran <martin.andersson@attentec.se>
2017-12-22 18:17:31 +0100

    smhi
    rain est

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-import display
-import weatherservices.weatherservice
+from display import display
+import weatherservices.smhi
 import geodata
 
+
+
 coord = geodata.get_coordinates()
-weather = weatherservices.weatherservice.WeatherService(coord)
+weather = weatherservices.smhi.Smhi(coord)
 print("lat: {}, lon: {}".format(weather.lat, weather.lon))
 disp = display.Display()
 disp.show_ip
 disp.show_text("Rain Display")
 
-rain_est = weather.get_rain_estimates()[0:disp.columns]
-disp.show_bars(rain_est)
+rain_est = weather.get_rain_estimates()
+disp.show_bars(rain_est[0:disp.columns], color='blue')
--- weatherservices/weatherservice.py
+++ weatherservices/weatherservice.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
-
+import abc
 
 class WeatherService():
+    __metaclass__ = abc.ABCMeta
 
-    def __init__(self, coordinates, city="", country=""):
+    def __init__(self, coordinates, hours=12, city="", country=""):
         self.lat = coordinates[0]
         self.lon = coordinates[1]
+        self.hours = hours
         self.city = city
         self.country = country
 
     def get_coordinates(self):
         return (self.lat, self.lon)
 
+    @abc.abstractmethod
     def get_rain_estimates(self):
-        return [0] * 12
+        """Return a list with rain estimates for
+           the next self.hours hours"""
