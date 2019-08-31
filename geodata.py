4f1bdf3164131fb37aa6dbc3ef41739d3e789679
maran <martin.andersson@attentec.se>
2019-08-31 13:29:10 +0200

    geodata - new api http://ip-api.com/json
    Use http://ip-api.com/json to get IP and location
    instead since it doesn't require a token.

--- geodata.py
+++ geodata.py
@@ -4,6 +4,8 @@ cities = {
     'stockholm': (59.3333, 18.05),
     'linkoping': (58.41086, 15.62157),
     'lund': (55.7058, 13.1932),
+    'uppsala': (59.8586, 17.6389),
+    'norrkoping': (58.5877, 16.1924)
 }
 
 
@@ -16,20 +18,14 @@ class Location():
             self.coordinates = cities[city]
             self.ip = "IP unknown"
 
-    def get_token(self):
-        token = None
-        with open("token.txt", 'r') as f:
-            token = f.read()
-        return token.strip()
-
     def get_data(self):
-        ext_url = "http://ipinfo.io/json?token={}".format(self.get_token())
+        ext_url = "http://ip-api.com/json"
         data = requests.get(ext_url).json()
         self.city = data['city']
-        self.ip = data['ip']
-        self.coordinates = data['loc'].split(',')
-        self.lat = self.coordinates[0]
-        self.lon = self.coordinates[1]
+        self.ip = data['query']
+        self.lat = data['lat']
+        self.lon = data['lon']
+        self.coordinates = (self.lat, self.lon)
 
     def print(self):
         print(self.city)
